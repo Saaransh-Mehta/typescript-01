daf4c3da29dc0c8b8d5fba4a8e5adec926bc8b34
Saaransh-Mehta <saaransh1621m@gmail.com>
2024-12-06 00:35:07 +0530

    Typescript day-02

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const a = "Hello";
 console.log(a);
 const b = 23;
 console.log(a + b);
+let bd = "Happy Boy";
+const result = b;
+const user = {
+    name: "Saaransh"
+};
+const email = user.name;
+console.log(email);
+let userId = 334455;
+let bool = true;
+console.log(bool);
+let hero;
+const getHero = () => {
+    return "thor";
+};
+hero = getHero();
+console.log(hero);
--- index.ts
+++ index.ts
@@ -3,5 +3,25 @@ const a: string = "Hello"
 console.log(a)
 const b: Number = 23
 console.log(a + b)
+let bd:string = "Happy Boy"
+const result: Number = b  
+const user = {
+    name: "Saaransh"
+}
+const email = user.name
+console.log(email)
 
+let userId:number = 334455
+
+let bool:boolean = true
+console.log(bool)
+
+let hero :any;
+
+const getHero = ()=>{
+    return "thor"
+}
+
+hero = getHero();
+console.log(hero)
 export{}
